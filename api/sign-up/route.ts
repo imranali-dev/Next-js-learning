42bb049b4491a1b2069c604153f2fc06cef648dd
imranali <codewithcodesandbox13@gmail.com>
2024-05-07 23:18:58 +0500

    sign up api here and send email otp for verification

--- api/sign-up/route.ts
+++ api/sign-up/route.ts
@@ -1,53 +1,61 @@
-import { sendVerificationEmail } from "@/helpers/emailverification";
-import { dbconnection } from "@/lib/debconnection";
-import UserModel from "@/model/users.model";
+import { sendVerificationEmail } from '@/helpers/emailverification';
+import { dbconnection } from '@/lib/debconnection';
+import UserModel from '@/model/users.model';
 import bcrypt from 'bcryptjs';
-
 export async function POST(request: Request) {
   await dbconnection();
-
   try {
     const { username, email, password } = await request.json();
-
-    const existingVerifiedUserByUsername = await UserModel.findOne({
+    const usernameexist = await UserModel.findOne({
       username,
-      isVerified: true,
+      isVerified: true
     });
-
-    if (existingVerifiedUserByUsername) {
-      return Response.json(
-        {
-          success: false,
-          message: 'Username is already taken',
-        },
-        { status: 400 }
-      );
-    }
-
-    const existingUserByEmail = await UserModel.findOne({ email });
-    let verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
-
+    if (usernameexist) {
+      return Response.json({
+        success: false,
+        message: ` Username ${usernameexist.username} is already taken`,
+      }, {
+        status: 400
+      })
+    };
+    // here need to that is the best practise
+    const existingUserByEmail = await UserModel.findOne({ email })
+    // here i have the confution that why i am using tostring() also learn about how many digits
+    // this code will be send to email
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
     if (existingUserByEmail) {
-      if (existingUserByEmail.isVerified) {
+      // here we the login now in this code we are say that if user is exist and verfied then  message: 'User already exists with this email',
+      if(existingUserByEmail.isVerified){
         return Response.json(
           {
             success: false,
             message: 'User already exists with this email',
           },
-          { status: 400 }
+          { status: 500 }
         );
-      } else {
+        // if not verified the user then but email is exist then oversite thse things this will be confusing for me in future 
+        //  here i am thinking in most of if user not verify email the resend it 
+        // overwriting the password mujhe sahi nahi lg rha change krna ho ga 
+      }else{
         const hashedPassword = await bcrypt.hash(password, 10);
+        // now we are sayting that overwrite the existing hashed password  
         existingUserByEmail.password = hashedPassword;
         existingUserByEmail.verifyCode = verifyCode;
-        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
-        await existingUserByEmail.save();
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 36000);
+       await existingUserByEmail.save();
+
       }
-    } else {
+      return Response.json({
+        success: false,
+        message: ` email ${existingUserByEmail.email} is already taken`,
+      })
+    }
+
+    else {
+      // if is the newswest user then we create the new user
       const hashedPassword = await bcrypt.hash(password, 10);
       const expiryDate = new Date();
-      expiryDate.setHours(expiryDate.getHours() + 1);
-
+      expiryDate.setHours(expiryDate.getHours() + 1)
       const newUser = new UserModel({
         username,
         email,
@@ -58,25 +66,29 @@ export async function POST(request: Request) {
         isAcceptingMessages: true,
         messages: [],
       });
-
       await newUser.save();
-    }
+    };
 
-    // Send verification email
+
+
+    //  now this time to send the VerificationEmail using resend mail
+    // here first i was passing the email in object it was giving me the error then i send send it without object error solve
     const emailResponse = await sendVerificationEmail(
       email,
       username,
-      verifyCode
-    );
+      verifyCode,
+    )
     if (!emailResponse.success) {
-      return Response.json(
-        {
-          success: false,
-          message: emailResponse.message,
-        },
-        { status: 500 }
-      );
-    }
+      if (!emailResponse.success) {
+        return Response.json(
+          {
+            success: false,
+            message: emailResponse.message,
+          },
+          { status: 500 }
+        );
+      }
+    };
 
     return Response.json(
       {
@@ -85,14 +97,19 @@ export async function POST(request: Request) {
       },
       { status: 201 }
     );
+
+
   } catch (error) {
     console.error('Error registering user:', error);
-    return Response.json(
+    return Response.json({
+      success: false,
+      message: 'Error registering user',
+    },
       {
-        success: false,
-        message: 'Error registering user',
-      },
-      { status: 500 }
-    );
+        status: 500
+      })
   }
 }
+
+// const { username, email, password } = await request.json();
+// const hasedPassword = await bcrypt.hash(password,10)
--- helpers/emailverification.ts
+++ helpers/emailverification.ts
@@ -6,12 +6,17 @@ export async function sendVerificationEmail(
   email: string,
   username: string,
   verifyCode: string
-): Promise<ApiResponse> {
+):
+// here we retturn something why we are using in this way dont know
+// here basically this is demanding for the return
+// must must return the message 
+// this maybe interview question
+Promise<ApiResponse> {
   try {
     await resendapi.emails.send({
-      from: 'dev@hiteshchoudhary.com',
+      from: 'onboarding@resend.dev',
       to: email,
-      subject: 'Mystery Message Verification Code',
+      subject: 'Testing App Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
     return { success: true, message: 'Verification email sent successfully.' };
